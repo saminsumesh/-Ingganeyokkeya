ec3a6094ca917ee0263be43093f7139fe7524d60
MOH-NIHAAL <104961284+MOH-NIHAAL@users.noreply.github.com>
2022-05-07 22:43:22 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -79,7 +79,7 @@ async def start(client, message):
         await client.send_photo(
             photo="https://telegra.ph/file/f5d411fba25ecfa5197fe.jpg",
             chat_id=message.from_user.id,
-            caption="☆ ʜᴇʟʟᴏ {}\n\n☆ ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ ᴊᴏɪɴ ᴍʏ ᴜᴘᴅᴀᴛᴇ ᴄʜᴀɴɴᴇʟ ᴛᴏ ɢᴇᴛ ʏᴏᴜʀ ʀᴇsᴜʟᴛ ☆.",
+            caption="☆ ʜᴇʟʟᴏ {query.from_user.mention}\n\n☆ ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ ᴊᴏɪɴ ᴍʏ ᴜᴘᴅᴀᴛᴇ ᴄʜᴀɴɴᴇʟ ᴛᴏ ɢᴇᴛ ʏᴏᴜʀ ʀᴇsᴜʟᴛ ☆.",
             reply_markup=InlineKeyboardMarkup(btn),
             parse_mode="markdown"
             )
