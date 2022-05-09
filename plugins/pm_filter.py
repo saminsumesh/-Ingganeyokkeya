63ff8d530b70643b90c75567c1788091ef6ee411
MOH-NIHAAL <104961284+MOH-NIHAAL@users.noreply.github.com>
2022-05-09 18:32:45 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -832,97 +832,144 @@ async def cb_handler(client: Client, query: CallbackQuery):
         )
     elif query.data == "help":
         buttons = [[
-            InlineKeyboardButton('» ᴍᴀɴᴜᴀʟ ғɪʟᴛᴇʀ «', callback_data='manuelfilter'),
-            InlineKeyboardButton('» ᴀᴜᴛᴏ ғɪʟᴛᴇʀ «', callback_data='autofilter'),
-            InlineKeyboardButton('» ᴄᴏɴɴᴇᴄᴛɪᴏɴ «', callback_data='coct')
+            InlineKeyboardButton('𝙼𝙰-𝙵𝙸𝙻𝚃𝙴𝚁', callback_data='manuelfilter'),
+            InlineKeyboardButton('𝙰𝚄-𝙵𝙸𝙻𝚃𝙴𝚁', callback_data='autofilter'),
+            InlineKeyboardButton('𝙲𝙾𝙽𝙽𝙴𝙲𝚃𝙸𝙾𝙽', callback_data='coct')
             ],[
-            InlineKeyboardButton('» sᴏɴɢ «', callback_data='songs'),
-            InlineKeyboardButton('» ᴇxᴛʀᴇ «', callback_data='extra'),
-            InlineKeyboardButton("» ᴠɪᴅᴇᴏ «", callback_data='video')
+            InlineKeyboardButton('𝚃𝙴𝙻𝙴𝙶𝚁𝙰𝙿𝙷', callback_data='tele'),
+            InlineKeyboardButton('𝚂𝚃𝙸𝙲𝙺𝙴𝚁-𝙸𝙳', callback_data='sticker'),
+            InlineKeyboardButton('𝚈𝚃-𝚃𝙷𝚄𝙼𝙱', callback_data='ytthumb')
             ],[
-            InlineKeyboardButton('» ᴘɪɴ «', callback_data='pin'), 
-            InlineKeyboardButton('» ᴘᴀsᴛᴇ «', callback_data='pastes'),
-            InlineKeyboardButton("» ɪᴍᴀɢᴇ «", callback_data='image')
+            InlineKeyboardButton('𝙵𝙸𝙻𝙴-𝚂𝚃𝙾𝚁𝙴', callback_data='newdata'),
+            InlineKeyboardButton('𝙰𝚄𝙳𝙸𝙾-𝙱𝙾𝙾𝙺', callback_data='abook'),
+            InlineKeyboardButton('𝚁𝙴𝙿𝙾𝚁𝚃', callback_data='report')
             ],[
-            InlineKeyboardButton('» ғᴜɴ «', callback_data='fun'), 
-            InlineKeyboardButton('» ᴊsᴏɴᴇ «', callback_data='son'),
-            InlineKeyboardButton('» ᴛᴛs «', callback_data='ttss')
-            ],[
-            InlineKeyboardButton('» ᴘᴜʀɢᴇ «', callback_data='purges'),
-            InlineKeyboardButton('» ᴘɪɴɢ «', callback_data='pings'),
-            InlineKeyboardButton('» ᴛᴇʟᴇɢʀᴀᴘʜ «', callback_data='tele')
-            ],[
-            InlineKeyboardButton('» ᴡʜᴏ ɪs «', callback_data='whois'),
-            InlineKeyboardButton('» ᴍᴜᴛᴇ «', callback_data='restric'),
-            InlineKeyboardButton('» ᴋɪᴄᴋ «', callback_data='zombies')
-            ],[
-            InlineKeyboardButton('» ʀᴇᴘᴏʀᴛ «', callback_data='report'),
-            InlineKeyboardButton('» ʏᴛ-ᴛʜᴜᴍᴘ «', callback_data='ytthumb'),
-            InlineKeyboardButton('» sᴛɪᴄᴋᴇʀ-ɪᴅ «', callback_data='sticker')
-            ],[
-            InlineKeyboardButton('» ᴄᴏᴠɪᴅ «', callback_data='corona'),
-            InlineKeyboardButton('» ᴀᴜᴅɪᴏ-ʙᴏᴏᴋ «', callback_data='abook'),
-            InlineKeyboardButton('» ᴜʀʟ-sʜᴏʀᴛᴇʀ «', callback_data='urlshort')
-            ],[
-            InlineKeyboardButton('» ɢ-ᴛʀᴀɴs «', callback_data='gtrans'),
-            InlineKeyboardButton('» ғɪʟᴇ-sᴛᴏʀᴇ «', callback_data='newdata'),
-            InlineKeyboardButton('» sᴛᴀᴛᴜs «', callback_data='stats')
-            ],[
-            InlineKeyboardButton('✨ ᴏᴡɴᴇʀ ✨', url='https://t.me/NL_MP4')
-            ],[
-            InlineKeyboardButton('⪻ ʙᴀᴄᴋ', callback_data='start')
+            InlineKeyboardButton('‹‹ 𝙱𝙰𝙲𝙺', callback_data='start'),
+            InlineKeyboardButton('𝚂𝚃𝙰𝚃𝚄𝚂', callback_data='stats'),
+            InlineKeyboardButton('𝙽𝙴𝚇𝚃 ››', callback_data='eth')
         ]]
-        reply_markup = InlineKeyboardMarkup(buttons)
-        await query.message.edit_text(
+        reply_markup = InlineKeyboardMarkup(buttons)       
+        await query.message.edit_text(                     
+            text=script.HELP_TXT.format(query.from_user.mention),
+            reply_markup=reply_markup,
+            parse_mode='html'
+        )
+    elif query.data == "eth":
+        buttons = [[ 
+            InlineKeyboardButton('𝙶-𝚃𝚁𝙰𝙽𝚂', callback_data='gtrans'),
+            InlineKeyboardButton('𝚄𝚁𝙻-𝚂𝙷𝙾𝚁𝚃', callback_data='urlshort'),
+            InlineKeyboardButton('𝙴𝚇𝚃𝚁𝙰', callback_data='extra')
+            ],[
+            InlineKeyboardButton('𝚂𝙾𝙽𝙶', callback_data='songs'),
+            InlineKeyboardButton('𝚃𝚃𝚂', callback_data='ttss'),
+            InlineKeyboardButton("𝚅𝙸𝙳𝙴𝙾", callback_data='video')            
+            ],[
+            InlineKeyboardButton("𝙸𝙼𝙰𝙶𝙴", callback_data='image'),
+            InlineKeyboardButton('𝙿𝚄𝚁𝙶𝙴', callback_data='purges'),
+            InlineKeyboardButton('𝚆𝙷𝙾𝙸𝚂', callback_data='whois')           
+            ],[
+            InlineKeyboardButton('‹‹ 𝙱𝙰𝙲𝙺', callback_data='help'),
+            InlineKeyboardButton('𝚂𝚃𝙰𝚃𝚄𝚂', callback_data='statsx'),
+            InlineKeyboardButton('𝙽𝙴𝚇𝚃 ››', callback_data='prop')
+        ]]
+        reply_markup = InlineKeyboardMarkup(buttons)       
+        await query.message.edit_text(                     
+            text=script.HELP_TXT.format(query.from_user.mention),
+            reply_markup=reply_markup,
+            parse_mode='html'
+        )  
+    elif query.data == "prop":
+        buttons = [[ 
+            InlineKeyboardButton('𝙹𝚂𝙾𝙽𝙴', callback_data='son'),
+            InlineKeyboardButton('𝙿𝙰𝚂𝚃𝙴', callback_data='pastes'),
+            InlineKeyboardButton('𝙲𝙾𝚅𝙸𝙳', callback_data='corona')
+            ],[
+            InlineKeyboardButton('𝙺𝙸𝙲𝙺', callback_data='zombies'),
+            InlineKeyboardButton('𝙿𝙸𝙽𝙶', callback_data='pings'),
+            InlineKeyboardButton('𝙼𝚄𝚃𝙴', callback_data='restric')            
+            ],[
+            InlineKeyboardButton('𝙵𝚄𝙽', callback_data='fun'), 
+            InlineKeyboardButton('𝙳𝙴𝙿𝙻𝙾𝚈', callback_data='deploy'),
+            InlineKeyboardButton('𝙿𝙸𝙽', callback_data='pin')           
+            ],[
+            InlineKeyboardButton('‹‹ 𝙱𝙰𝙲𝙺', callback_data='eth'),
+            InlineKeyboardButton('𝚂𝚃𝙰𝚃𝚄𝚂', callback_data='stats'),
+            InlineKeyboardButton('𝙷𝙾𝙼𝙴', callback_data='start')
+        ]]
+        reply_markup = InlineKeyboardMarkup(buttons)       
+        await query.message.edit_text(                     
             text=script.HELP_TXT.format(query.from_user.mention),
             reply_markup=reply_markup,
             parse_mode='html'
         )
     elif query.data == "hellp":
         buttons = [[
-            InlineKeyboardButton('» ᴍᴀɴᴜᴀʟ ғɪʟᴛᴇʀ «', callback_data='manuelfilter'),
-            InlineKeyboardButton('» ᴀᴜᴛᴏ ғɪʟᴛᴇʀ «', callback_data='autofilter'),
-            InlineKeyboardButton('» ᴄᴏɴɴᴇᴄᴛɪᴏɴ «', callback_data='coct')
+            InlineKeyboardButton('𝙼𝙰-𝙵𝙸𝙻𝚃𝙴𝚁', callback_data='manuelfilter'),
+            InlineKeyboardButton('𝙰𝚄-𝙵𝙸𝙻𝚃𝙴𝚁', callback_data='autofilter'),
+            InlineKeyboardButton('𝙲𝙾𝙽𝙽𝙴𝙲𝚃𝙸𝙾𝙽', callback_data='coct')
             ],[
-            InlineKeyboardButton('» sᴏɴɢ «', callback_data='songs'),
-            InlineKeyboardButton('» ᴇxᴛʀᴇ «', callback_data='extra'),
-            InlineKeyboardButton("» ᴠɪᴅᴇᴏ «", callback_data='video')
+            InlineKeyboardButton('𝚃𝙴𝙻𝙴𝙶𝚁𝙰𝙿𝙷', callback_data='tele'),
+            InlineKeyboardButton('𝚂𝚃𝙸𝙲𝙺𝙴𝚁-𝙸𝙳', callback_data='sticker'),
+            InlineKeyboardButton('𝚈𝚃-𝚃𝙷𝚄𝙼𝙱', callback_data='ytthumb')
             ],[
-            InlineKeyboardButton('» ᴘɪɴ «', callback_data='pin'), 
-            InlineKeyboardButton('» ᴘᴀsᴛᴇ «', callback_data='pastes'),
-            InlineKeyboardButton("» ɪᴍᴀɢᴇ «", callback_data='image')
+            InlineKeyboardButton('𝙵𝙸𝙻𝙴-𝚂𝚃𝙾𝚁𝙴', callback_data='newdata'),
+            InlineKeyboardButton('𝙰𝚄𝙳𝙸𝙾-𝙱𝙾𝙾𝙺', callback_data='abook'),
+            InlineKeyboardButton('𝚁𝙴𝙿𝙾𝚁𝚃', callback_data='report')
             ],[
-            InlineKeyboardButton('» ғᴜɴ «', callback_data='fun'), 
-            InlineKeyboardButton('» ᴊsᴏɴᴇ «', callback_data='son'),
-            InlineKeyboardButton('» ᴛᴛs «', callback_data='ttss')
-            ],[
-            InlineKeyboardButton('» ᴘᴜʀɢᴇ «', callback_data='purges'),
-            InlineKeyboardButton('» ᴘɪɴɢ «', callback_data='pings'),
-            InlineKeyboardButton('» ᴛᴇʟᴇɢʀᴀᴘʜ «', callback_data='tele')
-            ],[
-            InlineKeyboardButton('» ᴡʜᴏ ɪs «', callback_data='whois'),
-            InlineKeyboardButton('» ᴍᴜᴛᴇ «', callback_data='restric'),
-            InlineKeyboardButton('» ᴋɪᴄᴋ «', callback_data='zombies')
-            ],[
-            InlineKeyboardButton('» ʀᴇᴘᴏʀᴛ «', callback_data='report'),
-            InlineKeyboardButton('» ʏᴛ-ᴛʜᴜᴍᴘ «', callback_data='ytthumb'),
-            InlineKeyboardButton('» sᴛɪᴄᴋᴇʀ-ɪᴅ «', callback_data='sticker')
-            ],[
-            InlineKeyboardButton('» ᴄᴏᴠɪᴅ «', callback_data='corona'),
-            InlineKeyboardButton('» ᴀᴜᴅɪᴏ-ʙᴏᴏᴋ «', callback_data='abook'),
-            InlineKeyboardButton('» ᴜʀʟ-sʜᴏʀᴛᴇʀ «', callback_data='urlshort')
-            ],[
-            InlineKeyboardButton('» ɢ-ᴛʀᴀɴs «', callback_data='gtrans'),
-            InlineKeyboardButton('» ғɪʟᴇ-sᴛᴏʀᴇ «', callback_data='newdata'),
-            InlineKeyboardButton('» sᴛᴀᴛᴜs «', callback_data='stats')
-            ],[
-            InlineKeyboardButton('✨ ᴏᴡɴᴇʀ ✨', url='https://t.me/NL_MP4')
-            ],[
-            InlineKeyboardButton('⪻ ʙᴀᴄᴋ', callback_data='start')
+            InlineKeyboardButton('‹‹ 𝙱𝙰𝙲𝙺', callback_data='start'),
+            InlineKeyboardButton('𝚂𝚃𝙰𝚃𝚄𝚂', callback_data='stats'),
+            InlineKeyboardButton('𝙽𝙴𝚇𝚃 ››', callback_data='eth')
         ]]
-        reply_markup = InlineKeyboardMarkup(buttons)
-        await query.answer("☆ Wᴇʟᴄᴏᴍᴇ Tᴏ Mʏ Hᴇʟᴘ Mᴏᴅᴜʟᴇ ☆")
-        await query.message.edit_text(
+        reply_markup = InlineKeyboardMarkup(buttons)       
+        await query.message.edit_text(                     
+            text=script.HELP_TXT.format(query.from_user.mention),
+            reply_markup=reply_markup,
+            parse_mode='html'
+        )
+    elif query.data == "eth":
+        buttons = [[ 
+            InlineKeyboardButton('𝙶-𝚃𝚁𝙰𝙽𝚂', callback_data='gtrans'),
+            InlineKeyboardButton('𝚄𝚁𝙻-𝚂𝙷𝙾𝚁𝚃', callback_data='urlshort'),
+            InlineKeyboardButton('𝙴𝚇𝚃𝚁𝙰', callback_data='extra')
+            ],[
+            InlineKeyboardButton('𝚂𝙾𝙽𝙶', callback_data='songs'),
+            InlineKeyboardButton('𝚃𝚃𝚂', callback_data='ttss'),
+            InlineKeyboardButton("𝚅𝙸𝙳𝙴𝙾", callback_data='video')            
+            ],[
+            InlineKeyboardButton("𝙸𝙼𝙰𝙶𝙴", callback_data='image'),
+            InlineKeyboardButton('𝙿𝚄𝚁𝙶𝙴', callback_data='purges'),
+            InlineKeyboardButton('𝚆𝙷𝙾𝙸𝚂', callback_data='whois')           
+            ],[
+            InlineKeyboardButton('‹‹ 𝙱𝙰𝙲𝙺', callback_data='help'),
+            InlineKeyboardButton('𝚂𝚃𝙰𝚃𝚄𝚂', callback_data='statsx'),
+            InlineKeyboardButton('𝙽𝙴𝚇𝚃 ››', callback_data='prop')
+        ]]
+        reply_markup = InlineKeyboardMarkup(buttons)       
+        await query.message.edit_text(                     
+            text=script.HELP_TXT.format(query.from_user.mention),
+            reply_markup=reply_markup,
+            parse_mode='html'
+        )  
+    elif query.data == "prop":
+        buttons = [[ 
+            InlineKeyboardButton('𝙹𝚂𝙾𝙽𝙴', callback_data='son'),
+            InlineKeyboardButton('𝙿𝙰𝚂𝚃𝙴', callback_data='pastes'),
+            InlineKeyboardButton('𝙲𝙾𝚅𝙸𝙳', callback_data='corona')
+            ],[
+            InlineKeyboardButton('𝙺𝙸𝙲𝙺', callback_data='zombies'),
+            InlineKeyboardButton('𝙿𝙸𝙽𝙶', callback_data='pings'),
+            InlineKeyboardButton('𝙼𝚄𝚃𝙴', callback_data='restric')            
+            ],[
+            InlineKeyboardButton('𝙵𝚄𝙽', callback_data='fun'), 
+            InlineKeyboardButton('𝙳𝙴𝙿𝙻𝙾𝚈', callback_data='deploy'),
+            InlineKeyboardButton('𝙿𝙸𝙽', callback_data='pin')           
+            ],[
+            InlineKeyboardButton('‹‹ 𝙱𝙰𝙲𝙺', callback_data='eth'),
+            InlineKeyboardButton('𝚂𝚃𝙰𝚃𝚄𝚂', callback_data='statsy'),
+            InlineKeyboardButton('𝙷𝙾𝙼𝙴', callback_data='start')
+        ]]
+        reply_markup = InlineKeyboardMarkup(buttons)       
+        await query.message.edit_text(                     
             text=script.HELP_TXT.format(query.from_user.mention),
             reply_markup=reply_markup,
             parse_mode='html'
