f136e4a7df5776ef115c51ef49886c1e86718882
Rishi <64667088+rishye@users.noreply.github.com>
2022-05-18 16:08:17 +0530

    Update info.py

--- info.py
+++ info.py
@@ -74,6 +74,10 @@ DATABASE_URI = environ.get('DATABASE_URI', "")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Rajappan")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
+# Second database
+DATABASE_URI2 = environ.get('DATABASE_URI2', "")
+DATABASE_NAME2 = environ.get('DATABASE_NAME2', "Rajappan")
+
 #Downloader
 DOWNLOAD_LOCATION = environ.get("DOWNLOAD_LOCATION", "./DOWNLOADS/AudioBoT/")
 
